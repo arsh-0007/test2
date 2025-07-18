9e7f66724ac55e2ed7c39afb8474e7b7c29dff1e
arsh <arshmohd99@gmail.com>
2025-07-18 17:32:49 +0530

    second

--- src/main/java/com/example/test4/DemoApplication.java
+++ src/main/java/com/example/test4/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+	int y=34;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
